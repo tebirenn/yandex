b9d2de581b4e519ad55716719ab7ed8cf83f721e
Alan <a_saudabekov@kbtu.kz>
2022-12-10 00:47:12 +0600

    quick start

---
+++ src/pages/MarketMain/MarketMainPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./MarketMainPage.css";
+
+const MarketMainPage = () => {
+    return (
+        <section id="market-main">
+
+        </section>
+    );
+}
+
+export default MarketMainPage;
