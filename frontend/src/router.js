f772f98f97c1ec986ab326330e4372f88d336734
Alan <a_saudabekov@kbtu.kz>
2022-12-26 02:25:57 +0600

    profile page

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -3,8 +3,9 @@ import MarketMainPage from "./pages/MarketMainPage/MarketMainPage";
 import TravelMainPage from "./pages/TravelMainPage/TravelMainPage";
 import AuthPage from "./pages/AuthPage/AuthPage";
 
-import { AUTH_ROUTE, MARKET_MAIN_ROUTE, REGISTER_ROUTE, SERVICES_ROUTE, TRAVEL_MAIN_PAGE_ROUTE } from "./utils/consts";
+import { AUTH_ROUTE, MARKET_MAIN_ROUTE, PROFILE_ROUTE, REGISTER_ROUTE, SERVICES_ROUTE, TRAVEL_MAIN_PAGE_ROUTE } from "./utils/consts";
 import RegisterPage from "./pages/RegisterPage/RegisterPage";
+import ProfilePage from "./pages/ProfilePage/ProfilePage";
 
 export const publicRoutes = [
     {
@@ -26,5 +27,9 @@ export const publicRoutes = [
     {
         path: REGISTER_ROUTE,
         Component: RegisterPage
-    }
+    },
+    {
+        path: PROFILE_ROUTE,
+        Component: ProfilePage
+    },
 ]
